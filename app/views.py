394e5e7849dad7129efffcaea774528afe04e1a8
trinadhkesana <kesanatrinadh@gmail.com>
2023-11-09 22:05:21 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+def trinadh(request):
+    return render(request,'trinadh.html')
+def sai(request):
+    return render(request,'sai.html')
+def teja(request):
+    return render(request,'teja.html')
+def hemanth(request):
+     return render(request,'hemanth.html')
+
+# Create your views here.
